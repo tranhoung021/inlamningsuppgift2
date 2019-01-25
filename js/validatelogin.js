d22a635c414ec1c091676d80fb0310951448a75b
Houng <majinht@hotmail.com>
2019-01-25 10:55:51 +0100

    updated login

--- js/validatelogin.js
+++ js/validatelogin.js
@@ -15,10 +15,8 @@ $(function () {
                         window.location.href = "index.html";
                     } else {
                         $("body").prepend(`<div id="alert-box" class="position-absolute w-100 text-center"><div class="alert alert-danger" role="alert">Incorrect user or password</div></div>`);
-                        $("#alert-box").fadeIn("fast").delay(1000).fadeOut("slow");
+                        $("#alert-box").fadeIn("fast").delay(2000).fadeOut("slow");
                     }
-
-
                 });
 
             }
@@ -34,11 +32,9 @@ $(function () {
                 if ($(input).attr("type") === "password" && $(input).prop("required")) {
         
                     min = 1;
-                    validateInputValue(input, `Requires a minimum of ${min} characters`, min);
+                    validateInputValue(input);
         
-                    if (!$(input).val() && !$(input).prop("required")) {
-                        isValid(input);
-                    }
+                    
                 }
             }
         
@@ -57,7 +53,7 @@ $(function () {
             function validateEmail(input) {
                 let invalidFeedbackId = "#" + $(input).attr("id") + "-invalid-feedback";
                 let invalidFeedbackDefault = $(invalidFeedbackId).html();
-                let invalidFeedback = "Please enter a valid E-Mail";
+                let invalidFeedback = "The E-Mail you entered is not valid";
                 let regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,6})+$/;
         
                 if (!$(input).val()) {
@@ -70,17 +66,14 @@ $(function () {
                     isValid(input);
                 }
             }
-            function validateInputValue(input, error, min = 1, max = 4096) {
+            function validateInputValue(input) {
                 let invalidFeedbackId = "#" + $(input).attr("id") + "-invalid-feedback";
                 let invalidFeedbackDefault = $(invalidFeedbackId).html();
-                let invalidFeedback = error;
         
                 if (!$(input).val()) {
                     isInvalid(input);
                     $(invalidFeedbackId).html(invalidFeedbackDefault);
-                } else if ($(input).val().length < min) {
-                    isInvalid(input);
-                    $(invalidFeedbackId).html(invalidFeedback);
+                
                 } else {
                     isValid(input);
                 }
